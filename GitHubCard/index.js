9cc3325972769c83b4c7f638fffefb519ac16856
Gideon <reachgiddy@gmail.com>
2020-01-16 19:05:42 -0500

    created the cardsCreator function

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,9 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+// axios.get("https://api.github.com/users/GideonOgbuagu").then(response => {
+//   console.log(response);
+// });
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -45,6 +47,75 @@ const followersArray = [];
 </div>
 
 */
+function cardsCreator(users) {
+  const card = document.createElement("div"),
+    cardImg = document.createElement("img"),
+    cardInfo = document.createElement("div"),
+    name = document.createElement("h3"),
+    username = document.createElement("p"),
+    location = document.createElement("p"),
+    profile = document.createElement("p"),
+    link = document.createElement("a"),
+    followers = document.createElement("p"),
+    following = document.createElement("p"),
+    bio = document.createElement("p");
+
+  card.classList.add("card");
+  cardImg.src = users.imgUser;
+  cardInfo.classList.add("class-info");
+  name.classList.add("name");
+  username.classList.add("username");
+  link.href = users.addressUser;
+
+  name.textContent = users.nameUser;
+  username.textContent = users.username;
+  location.textContent = users.locationUser;
+  link.textContent = users.addressUser;
+  followers.textContent = users.followersUser;
+  following.textContent = users.followingUser;
+  bio.textContent = users.bioUser;
+
+  card.append(cardImg);
+  card.append(cardInfo);
+  cardInfo.append(name);
+  cardInfo.append(username);
+  cardInfo.append(location);
+  cardInfo.append(profile);
+  profile.append(link);
+  cardInfo.append(followers);
+  cardInfo.append(following);
+  cardInfo.append(bio);
+
+  return card;
+}
+
+const cardContainer = document.querySelector(".card");
+
+// const newComponent = document.createElement("div");
+
+// cards.append();
+
+axios.get("https://api.github.com/users/GideonOgbuagu").then(response => {
+  //       console.log(response.data.followers_url)
+  axios.get(response.data.followers_url).then(response => {
+    // console.log(response.data);
+    response.data.forEach(item => {
+      axios.get(item.url).then(response => {
+        // response.data.forEach(follower => {
+        console.log(response.data);
+        //   // const cards = cardsCreator()
+        //   // cardContainer.append();
+        // });
+      });
+    });
+  });
+});
+
+// https://api.github.com/users/GideonOgbuagu/followers
+
+// axios.get("https://api.github.com/users/GideonOgbuagu/followers").then(response => {
+//   console.log(response);
+// });
 
 /* List of LS Instructors Github username's: 
   tetondan
